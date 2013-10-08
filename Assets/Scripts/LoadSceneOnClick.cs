9f96fca0f6d169df8cd6c8732e4124196d79633a
LucasRowe <lucas.rowe@gmail.com>
2013-10-08 16:14:51 -0400

    Title screen menu added with scene change.

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceneOnClick : MonoBehaviour {
+	
+	public string sceneName;
+
+	void OnClick ()
+	{
+		if (!string.IsNullOrEmpty(sceneName))
+		{
+			Application.LoadLevel(sceneName);
+		} else {
+			Debug.LogError ("Scene name not set. Scene not loaded.");
+		}
+	}
+}
