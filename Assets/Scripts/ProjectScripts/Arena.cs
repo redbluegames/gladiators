451d2866e783f8b75e1d78bb9503479278300361
LucasRowe <lucas.rowe@gmail.com>
2013-10-18 15:16:28 -0400

    First checkin of Singleton Code for Manager classes.
    Includes Arena and Manager classes, both singleton
    manager-types. Roles of each to be determined.

---
+++ Assets/Scripts/ProjectScripts/Arena.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arena : Singleton<Arena>
+{
+	public int totalWaves = 10;
+	public int curWave = 0;
+	public int[] waveComp = {2,4,6,8,10,12,14,16,18,20};
+	
+	public Transform spawnPoint;
+	
+	// guarantee this will be always a singleton only - can't use the constructor!
+	protected Arena ()
+	{
+	}
+	
+	public void StartNextWave ()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/ProjectScripts/Manager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Code pattern for this class borrowed from:
+ * http://wiki.unity3d.com/index.php/Singleton
+ *
+ * This is our "god class" for handling managers and game-global variables.
+ **/
+public class Manager : Singleton<Manager>
+{
+	// guarantee this will be always a singleton only - can't use the constructor!
+	protected Manager ()
+	{
+	}
+	
+	public Arena arena;
+ 
+	void Awake ()
+	{
+//		arena = Instance.GetComponent<Arena> ();
+		if (arena == null) {
+			Debug.Log ("Adding Arena Component to Manager since it wasn't attached via the Editor.");
+//			arena = gameObject.AddComponent<Arena> ();
+		}
+	}
+}
